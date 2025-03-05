7f8c9038012b67a454970c71f81a641f180692e4
Brandon Knutson <bknutson@mines.edu>
2025-03-04 19:47:49 -0700

    begin it_net training

--- src/train.py
+++ src/train.py
@@ -10,8 +10,12 @@ def main() -> None:
     hyperparams.train_jfb = False
 
     # Define percolation values for fine-tuning
-    # percolations = [0.000, 0.001, 0.003, 0.01, 0.03, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
-    percolations = [0.5]
+    # percolations = [0.000, 0.001, 0.003]  # job:1
+    # percolations = [0.01, 0.03, 0.1] # job:2
+    # percolations = [0.2, 0.3, 0.4] # job:3
+    percolations = [0.5, 0.6, 0.7]  # cuda:0
+    # percolations = [0.8, 0.9, 0.99]  # job:4
+
     # Train the model for each percolation value
     for percolation in percolations:
         hyperparams.percolation = percolation
