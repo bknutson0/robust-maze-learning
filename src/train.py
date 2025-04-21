a3779aa0f8fa9dda2a67af4b242eafc64711ae00
Brandon Knutson <bknutson@mines.edu>
2025-04-20 20:41:16 -0600

    prepare for ff-net training run

--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ def main() -> None:
     hyperparams.model_name = 'ff_net'
     hyperparams.pretrained = None  #'models/it_net/2025-03-23_12:27:31/checkpoints/epoch_10.pth'
     hyperparams.iters = 30
-    hyperparams.num_mazes = int(1e3)  # TODO: change back to 1e5
+    hyperparams.num_mazes = int(1e5)
     hyperparams.random_iters = False
     hyperparams.contraction = None
     hyperparams.train_jfb = False
@@ -25,10 +25,10 @@ def main() -> None:
     hyperparams.patience = 10
 
     # Define percolation values for fine-tuning
-    # percolations = [0.000, 0.001, 0.003, 0.01]  # cuda:1
-    # percolations = [0.03, 0.1, 0.2, 0.3]  # cuda:2
-    percolations = [0.4, 0.5, 0.6, 0.7]  # job:1
-    # percolations = [0.8, 0.9, 0.99]  # job:2
+    percolations = [0.000, 0.001, 0.003, 0.01]  # job:1
+    # percolations = [0.03, 0.1, 0.2, 0.3]  # job:2
+    # percolations = [0.4, 0.5, 0.6, 0.7]  # job:3
+    # percolations = [0.8, 0.9, 0.99]  # job:4
 
     # Train the model for each percolation value
     for percolation in percolations:
